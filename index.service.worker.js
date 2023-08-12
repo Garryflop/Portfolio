f9a5e4905508c122dfa88347f0b6aa573007326e
Harryfloppa <97098278+Garryflop@users.noreply.github.com>
2023-08-12 14:46:18 +0600

    Add files via upload

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1686723775|3113210850";
+const CACHE_VERSION = "1691829869|3879678687";
 const CACHE_PREFIX = "ProfileGame-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
